ed8d55c97575100829d63e7fa3ecefe7246b40a3
Leon Jamison <leonjamison@icloud.com>
2019-11-25 20:38:54 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,10 @@
-# code your #position_taken? method here!
+def position_taken?
+  array[index_number]
+  if "[index_number] != [" "]"
+    puts !=
+  end
+  board[index]
+  if "[index] == [" "] || [""] || [nil]"
+    puts !=
+  end
+end
