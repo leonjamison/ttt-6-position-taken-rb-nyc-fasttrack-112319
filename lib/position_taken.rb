40d15660488dfc8f7757b49768f72d41ac8208f7
Leon Jamison <leonjamison@icloud.com>
2019-11-25 20:36:02 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,8 @@
 def position_taken?
-  array[index_number]
-  if "[index_number] != [" "]"
-    puts !=
-  end
+  # array[index_number]
+  # if "[index_number] != [" "]"
+  #   puts !=
+  # end
   board[index]
   if "[index] == [" "] | | [""]"
     puts !=
